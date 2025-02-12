3cf88d5d6400d108b5694da7e4e9a6fbcdb35c46
WolFyXXC <147456051+WolFyXXC@users.noreply.github.com>
2025-02-12 14:30:52 +0300

    Добавьте файлы проекта.

---
+++ Algoritmandprog/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmandprog
+{
+    internal class Balance
+    {
+        private readonly int balance = 38000;
+
+        public int B
+        {
+            get { return balance; }
+            //set { balance = value; }
+            //public int GetBalance()
+            //{
+            //    return balance;
+            //}
+
+            //public void SetBalance(int balance)
+            //{
+            //    balance = value;
+            //}
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            Balance balance = new Balance();
+            int numberofscore = 0;         
+            int scorenumb = score.Score(numberofscore);
+            Console.WriteLine("Сколько денег у вас на счете? ");
+            //balance.B = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine($"\nНомер счета: {scorenumb}");
+
+            Console.WriteLine($"Баланс счета: {balance.B}");
+        }
+    }
+}
---
+++ Algoritmandprog/score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmandprog
+{
+    internal class score
+    {
+        public static int Score (int numberofscore)
+        {
+            Random Random = new Random();
+            numberofscore = Random.Next(1000, 10000);
+            return numberofscore;
+        }
+       
+
+
+    }
+}
